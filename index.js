ece6bf829b4cbb24e9882af79b0bd068767d823f
Pedro Macena <110847027+somemacsongs@users.noreply.github.com>
2022-09-28 16:12:10 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { connect } from "./config/db.config.js";
+import { userRouter } from "./routes/user.routes.js";
+import cors from "cors";
+
+dotenv.config();
+connect();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_VERSION = "1.0";
+
+app.use(`/api/${API_VERSION}/user`, userRouter);
+
+app.listen(Number(process.env.PORT), () => {
+  console.log(`Server up and running at port ${process.env.PORT}`);
+});
