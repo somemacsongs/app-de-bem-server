a0fa59ba22011eebac87b7ffc1176f85bda5b94a
somemacsongs <pedromacenac@gmail.com>
2022-09-29 17:35:52 -0300

    starting comment routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { connect } from "./config/db.config.js";
 import { userRouter } from "./routes/user.routes.js";
+import { commentRouter } from "./routes/comment.routes.js"
 import cors from "cors";
 
 dotenv.config();
@@ -12,9 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const API_VERSION = "1.0";
-
-app.use(`/api/${API_VERSION}/user`, userRouter);
+app.use("/app-de-bem/users", userRouter);
+app.use("/app-de-bem/comments", commentRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and running at port ${process.env.PORT}`);
--- model/user.model.js
+++ model/user.model.js
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 
+const avatars = ["https://images2.imgbox.com/54/1f/hdwOJPTU_o.png","https://images2.imgbox.com/3d/d6/kfsaSCCt_o.png","https://images2.imgbox.com/65/0c/NKDImieB_o.png"]
+
 const userSchema = new Schema({
   name: { type: String, required: true, trim: true },
   email: {
@@ -12,6 +14,7 @@ const userSchema = new Schema({
   passwordHash: { type: String, required: true },
   role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
   createdAt: { type: Date, default: Date.now() },
+  avatar: {type: String, default:"https://images2.imgbox.com/54/1f/hdwOJPTU_o.png"}
 });
 
 export const UserModel = model("User", userSchema);
